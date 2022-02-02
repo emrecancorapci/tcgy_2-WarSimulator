d3af2f7e6490577c3a37872e21f4522f18b697a0
emrecancorapci <emrecancorapci@gmail.com>
2022-02-02 03:47:52 +0300

    Commit init

---
+++ SimulationConsole.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace tcgy_2_War_Simulator
+{
+    internal static class SimulationConsole
+    {
+        private static void WriteColored(string str, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        private static void WriteColoredLine(string str, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void AttackInfo(Soldier attacker, Soldier defender)
+        {
+            WriteColored($"{attacker.Name}({attacker.Health})", attacker.Color);
+            WriteColored(" is attacking to ", ConsoleColor.White);
+            WriteColored($"{defender.Name}({defender.Health})", defender.Color);
+            WriteColored(" with ", ConsoleColor.White);
+            WriteColored($"{attacker.Weapon.Name}!\n", ConsoleColor.Green);
+        }
+        public static void AttackOutput(Soldier defender, int amount)
+        {
+            WriteColored($"{defender.Name}", defender.Color);
+            WriteColored(" got ", ConsoleColor.White);
+            WriteColored($"{amount}", ConsoleColor.Red);
+            WriteColored(" damage!\n", ConsoleColor.White);
+        }
+        public static void SoldierInfo(Soldier soldier)
+        {
+            Console.WriteLine("===========================");
+
+            Console.Write(" Name : ");
+            WriteColoredLine($"{soldier.Name}", ConsoleColor.Blue);
+
+            Console.Write(" Weapon : ");
+            WriteColoredLine($"{soldier.Weapon.Name}", ConsoleColor.Red);
+
+            Console.Write(" Hit-point : ");
+            WriteColoredLine($"{soldier.HitPoint}", ConsoleColor.Yellow);
+
+            Console.WriteLine("===========================\n");
+        }
+        public static void DeadSoldierInfo(Soldier soldier)
+        {
+            WriteColoredLine($"{soldier.Name} is DEAD!", ConsoleColor.DarkRed);
+        }
+    }
+}
---
+++ Soldier.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace tcgy_2_War_Simulator
+{
+    internal class Soldier
+    {
+        public static int Id;
+        public Random Random = new();
+
+        public Weapon Weapon { get; }
+        public ConsoleColor Color { get; set; }
+        public int HitPoint { get; }
+        public int Health { get; private set; } = 100;
+        public int Armor { get; } = 2;
+        public string Name { get; set; }
+        public bool IsAlive { get; private set; } = true;
+
+        private const int HitPointUpLimit = 8;
+        private const int HitPointDownLimit = 1;
+
+
+        public Soldier()
+        {
+            NameSoldier();
+
+            HitPoint = Random.Next(HitPointDownLimit, HitPointUpLimit);
+            Weapon = new Weapon(Random.Next(0,3));
+            SimulationConsole.SoldierInfo(this);
+        }
+
+        public void Attack(Soldier enemy)
+        {
+            var damage = Random.Next(HitPoint);
+            SimulationConsole.AttackInfo(this, enemy);
+
+            var amount = enemy.GetDamage(damage + Weapon.Damage);
+            SimulationConsole.AttackOutput(enemy, amount);
+
+            if (enemy.Health >= 0) return;
+            SimulationConsole.DeadSoldierInfo(enemy);
+        }
+
+        public int GetDamage(int damageAmount)
+        {
+            if (damageAmount < Armor) return 0;
+            
+            Health -= damageAmount;
+
+            if (Health < 0) IsAlive = false;
+
+            return damageAmount;
+        }
+
+        private void NameSoldier()
+        {
+            Name = $"{++Id}. Soldier";
+        }
+    }
+}
